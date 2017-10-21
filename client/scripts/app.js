8ac51018540e0b6234e0d984faa3e79b2f2b8eb3
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-20 20:05:58 -0700

    completed root option

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 // YOUR CODE HERE:
 var app = {};
+app.room = [];
 app.server = 'http://parse.sfm8.hackreactor.com/';
 app.init = function() { 
 };
@@ -29,6 +30,7 @@ app.fetch = function() {
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     success: function (data) {
+      console.log(data);
       for (let message of data.results) {
         app.renderMessage(message);
       }
@@ -44,12 +46,18 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) { 
-  let child = '<p><span class="username">' + message.username + '</span>' + message.text + '</p>';
+  if (!app.room.includes(message.roomname)) {
+    app.room.push(message.roomname);
+    let roomOpt = '<option value=' + message.roomname + '>' + message.roomname + '</option>';
+    $('#chatrooms').append(roomOpt);
+  }
+  let child = '<p class=' + message.roomname + '><span class="username">' + message.username + ': ' + '</span>' + message.text + '</p>';
   $('#chats').append(child);
 };
 
 app.renderRoom = function(room) { 
-  let child = '<p>' + room + '</p>';
+  let child = '<option>' + room + '</option>';
+  
   $('#roomSelect').append(child);
 };
 
@@ -69,8 +77,15 @@ $(document).ready(function() {
   $('#send').on('submit', $('.submit'), function() {
     app.handleSubmit();
   });
-
+  var filterByRoom = function() {
+    let room = document.getElementById('chatrooms').value;
+    console.log(room);
+    let msgClass = '.' + room;
+    let filteredMsg = $(msgClass);
+    console.log(filteredMsg);
+  };
 });
+
 
 
 
